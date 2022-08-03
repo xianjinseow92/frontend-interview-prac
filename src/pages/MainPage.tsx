4ff5956e0837b429f2a9915888d15b3a1bc0c4fd
Xian Jin Seow <xianjin.seow@circles.asia>
2022-08-03 22:20:53 +0800

    create temporary main page

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,15 @@
+/**
+ * Contains all buttons to navigate to Interview Questions that have been done
+ */
+
+import Container from "@mui/material/Container";
+
+const MainPage = () => {
+    return (
+        <Container>
+            Buttons and shit here
+        </Container>
+    )
+};
+
+export default MainPage;

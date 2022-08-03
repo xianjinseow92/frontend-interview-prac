c94309f8841a273ed2a6d2c1f2734f1502aeac96
Xian Jin Seow <xianjin.seow@circles.asia>
2022-08-03 22:21:04 +0800

    create header component

---
+++ src/layout/Header.tsx
@@ -0,0 +1,33 @@
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import HomeIcon from "@mui/icons-material/Home";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import routes from "routes/routes";
+import find from "lodash/find";
+import get from "lodash/get";
+import { useNavigate } from "react-router-dom";
+
+const TopNavBar = () => {
+  const navigate = useNavigate();
+  const homePage: string | undefined = get(find(routes, {path: "/home"}), "path", "");
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          onClick={() => {
+            navigate(homePage);
+          }}
+        >
+          <HomeIcon />
+        </IconButton>
+        <Typography variant="h6">Frontend Interview Questions</Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default TopNavBar;

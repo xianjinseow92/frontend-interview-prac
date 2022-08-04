d53a48818d64a053638b3e9d0211a3bab7edfc60
Xian Jin Seow <xianjin.seow@circles.asia>
2022-08-04 15:10:49 +0800

    Complete main page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,15 +1,36 @@
 /**
  * Contains all buttons to navigate to Interview Questions that have been done
  */
-
-import Container from "@mui/material/Container";
+import { interviewQuestionRoutes } from "routes/routes";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import map from "lodash/map";
+import { IRoute } from "types/types";
+import { useNavigate } from "react-router-dom";
 
 const MainPage = () => {
+  const navigate = useNavigate();
+
+  const renderInterviewQuestionContainer = (route: IRoute) => {
     return (
-        <Container>
-            Buttons and shit here
-        </Container>
-    )
+      <Grid
+        item
+        xs={6}
+        md={4}
+        key={route.path}
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        <Button variant="outlined" onClick={() => navigate(route.path)}>
+          {route.path}
+        </Button>
+      </Grid>
+    );
+  };
+  return (
+    <Grid container sx={{ backgroundColor: "green" }} spacing={2}>
+      {map(interviewQuestionRoutes, renderInterviewQuestionContainer)}
+    </Grid>
+  );
 };
 
 export default MainPage;

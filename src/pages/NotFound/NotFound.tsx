96d5c365ed289425115a1a3a8bd79e161d4e17bb
Xian Jin Seow <xianjin.seow@circles.asia>
2022-08-04 14:36:51 +0800

    edit fallback page content

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -2,14 +2,17 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 
 const NotFound = () => {
-    return (
-        <Container sx={{height: "100%"}}>
-            <Typography variant="h1">
-                Bruh I don't think what you're looking for exists. You should probably head back to the home page.
-                Just click on the Home Icon in the navbar above.
-            </Typography>
-        </Container>
-    )
+  return (
+    <Container sx={{ height: "100%" }}>
+      <Typography variant="h5">
+        Bruh I don't think what you're looking for exists. You should probably
+        head back to the home page.
+      </Typography>
+      <Typography variant="h6" sx={{mt:5}}>
+        Just click on the Home Icon in the navbar above.
+      </Typography>
+    </Container>
+  );
 };
 
 export default NotFound;
